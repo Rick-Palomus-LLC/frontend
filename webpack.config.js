014d44f1524a7f590386ecf71ae03334833b26cb
Kevin <kevin@Kevins-MBP.fios-router.home>
2018-10-12 19:39:32 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const path = require('path');
+
+module.exports = {
+    mode: process.env.MODE,
+    entry: './app/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'assets/js'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.sass$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    }
+};
