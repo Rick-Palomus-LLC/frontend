ee5d50de34fc18feba51f58afa8cb3cea100552a
Kevin Diem <kg.diem@gmail.com>
2018-10-14 11:53:39 -0400

    Update stripeUtil.js

--- app/js/lib/stripeUtil.js
+++ app/js/lib/stripeUtil.js
@@ -1,33 +1,28 @@
-export default class StripeUtil {
+const stripe = Stripe('ENV.STRIPE_PUBLISHABLE_KEY');
+
+export default const StripeUtil = {
+    addCard: addCard,
+    submitPayment: submitPayment
+}
+
+function addCard(selector){
+    const elements = stripe.elements();
+        
+    const card = elements.create('card');
     
-    constructor(){
-        this._stripe = Stripe('ENV.STRIPE_PUBLISHABLE_KEY');
-    }
+    card.mount(selector);
+    
+    return card;
+}
 
-    addCard(selector){
-        if(this._card)
-            return this._card;
-        
-        const elements = this._stripe.elements();
-        
-        const card = elements.create('card');
+async function submitPayment(card){
+    if(!card)
+        throw new Error('Card is required');
+    
+    const {token, error} = await this._stripe.createToken(this._card);
+    
+    if(error)
+        throw new Error(error.message);
 
-        card.mount(selector);
-
-        this._card = card;
-
-        return card;
-    }
-
-    async submitPayment(){
-        if(!this._card)
-            return;
-
-        const {token, error} = await this._stripe.createToken(this._card);
-
-        if(error)
-            throw new Error(error.message);
-
-        return token;
-    }
+    return token;
 }
